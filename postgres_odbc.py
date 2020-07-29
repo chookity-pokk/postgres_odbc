4c4a62b9d42e5f44a86760e27efa35afae43acf2
Hank <hank@gddomain.net>
2020-07-29 16:11:32 -0700

    replaced dbconnection with cnxn and it still works, mission accomplished

--- postgres_odbc.py
+++ postgres_odbc.py
@@ -25,9 +25,6 @@ def connect_to_database():
     return cnxn, cursor
 
 # Specifying the ODBC driver, server name, database, etc. directly
-#cnxn = pyodbc.connect(databasename='testdb',databaseIP='localhost',databaseport='5432',username='postgres',password='postgres')
-
-#cnxn = pyodbc.connect('DRIVER={PostgreSQL Unicode};SERVER=localhost;DATABASE=testdb;UID=postgres;PWD=postgres')
 
 # Using a DSN, but providing a password as well
 #This will need to be adjusted for QuickBooks, I'll add a link when I find one
@@ -49,22 +46,22 @@ tb = str(input("Input name of the table: \n " ))
 This will create a table in the postgres database.
 """
 #changing newtablename to tb... I'll fix that later
-def create_table(dbconnection, cursor, tb):
+def create_table(cnxn, cursor, tb):
     sql = '''CREATE TABLE {0} (oid serial PRIMARY KEY)'''.format(tb)
     cursor.execute(sql)
-    dbconnection.commit()
+    cnxn.commit()
 #create_table(conn, cur, 'DeleteMe')
 
 """
 Takes the connection to the database and adds a new field information and updates the
 table with a new field.
 """
-def add_field(dbconnection, cursor, tablename, fieldname='newfield',fieldtype='TEXT'):
+def add_field(cnxn, cursor, tablename, fieldname='newfield',fieldtype='TEXT'):
     sql = '''ALTER TABLE {0} ADD COLUMN {1} {2}'''.format(tablename, fieldname, fieldtype)
     cursor.execute(sql)
-    dbconnection.commit()
+    cnxn.commit()
 
-def add_to_row(dbconnection, cursor, tablename,fieldnames):
+def add_to_row(cnxn, cursor, tablename,fieldnames):
     #sql = '''INSERT INTO {}(id,names) values(?,?)'''.format(tablename),'pyodbc','el biblioteca'
     #sql = "insert into {}(id, names) values (?,?)".format(tablename), 'pyodbc', 'awesome library'
     #cursor.execute(sql)
@@ -81,13 +78,13 @@ def add_to_row(dbconnection, cursor, tablename,fieldnames):
     #So this seems to work below here but above here is having a problem. It is literally
     #the same exact thing so I don't know what the hold up is.
     #cur.execute("insert into {}(id, names) values (?,?)".format(tb), 'pyodbc', 'awesome library')
-    dbconnection.commit()
+    cnxn.commit()
 """
 This will disconnect you from the database
 """
-def disconnect_from_database(dbconnection, cursor):
+def disconnect_from_database(cnxn, cursor):
     cursor.close()
-    dbconnection.close()
+    cnxn.close()
 
 """
 This is the main function, which is currently being used for testing purposes
@@ -96,6 +93,18 @@ disconnect and create tables in the postgres database. Fingers crossed. 7/28/202
 Ayo, seems to be working just fine now that I editted it to make the table name an input.
 Issue comes in when a table already exists with the name but that makes sense.
 """
+
+"""
+Notes on how to implement into QuickBooks. Seems like anything with '_line_inv'
+at the end of it is an inventory item. Check out the pdf sent by time on 7/29/2020
+if confused. (Add a link to pdf when available for future reference).
+_inv_adjust is inventory adjustments
+Honestly may have been able to just connect it through Microsoft Access. If that
+is the case then I may just write a much smaller script to launch that and
+have it pull the inventory daily. Page 17 of the report.
+
+"""
+
 def main():
     connect_to_database()
     tb = str(input("Input new table name: \n " ))
@@ -121,7 +130,7 @@ it was also adding entries into the columns to see if that was working and
 results were positive as it was able to add entries into the table.
 """
 
-#add_to_row(conn,cur,tb,fieldnames=fieldnames)
+add_to_row(conn,cur,tb,fieldnames=fieldnames)
 
 #cur.execute("insert into products(id, name) values (?,?)", 'pyodbc', 'awesome library')
 #cursor.commit()
