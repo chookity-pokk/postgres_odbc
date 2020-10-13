c83b9b5e0be1be32a9446ac1843fc09220bdb808
Hank <ragreenburg@gmail.com>
2020-10-13 10:21:59 -0700

    Added connection to db

--- parts.py
+++ parts.py
@@ -13,10 +13,40 @@ folder labeled 'Parts' just to
 organize the code
 """
 
-#[] TODO
+#[X] TODO
 #Connect each of these part scripts to the database.
 
 tb = "parts_db"
+
+def connect_to_database(
+    databasename="testdb",
+    databaseIP="localhost",
+    databaseport="5432",
+    username="postgres",
+    password="postgres",
+):
+    """Module takes arguments to connect to a PostgreSQL database using PostgreSQL and returns a connection.
+   Args:
+       databasename: Name of the database to connect. Default livingdb
+       databaseIP: IP address of the database server.
+       databaseport: Port of the database server
+       username: DB username
+       password: User password
+   Returns:
+       connection: connection to the database
+       cursor: cursor for database connection
+   """
+    # Create the connection
+    connection = psycopg2.connect(
+        host=databaseIP, user=username, password=password, dbname=databasename
+    )
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+conn, cur = connect_to_database()
+
+
 def parts_db():
     # --------------------------------------- Global Vars -------------------------------------------------------
     global parts
